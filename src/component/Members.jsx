c105d9bb31201d99420b1e95b0daac11e10f3e58
unknown <DEVANSH RAJVANSHI@DEV-PC.DEV>
2025-02-28 22:34:53 +0530

    done

--- src/component/Members.jsx
+++ src/component/Members.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { deleteCart } from './store/addToCart/slice';
+import Nav from './Nav';
 const Members = () => {
 
   const members = useSelector( state => state.cart.cartData)
@@ -8,10 +9,11 @@ const Members = () => {
 
   return (
     <div className='h-full w-full pt-8 bg-linear-to-r from-gray-900 via-blue-700 to-gray-900'>
+    <Nav/>
       <div className='flex flex-col mt-8 items-center justify-center gap-10'>
         {
-          members.map( item => {
-            return <div className="p-4 w-xl bg-green-400 rounded-full flex">
+            members.map( item => {
+                return <div className="p-4 w-xl bg-green-400 rounded-full flex">
                 <div>
                 <h1>{item.name}</h1>
                 <h3>{item.email}</h3>
--- src/component/Nav.jsx
+++ src/component/Nav.jsx
@@ -6,7 +6,10 @@ const Nav = () => {
   const members = useSelector( state => state.cart.cartData)
   const navigate = useNavigate()
   return (
-    <div className='flex justify-end px-4'>
+    <div className='flex justify-between px-4'>
+        <button onClick={()=>navigate('/')} className='text-white relative bg-green-700 p-2 rounded'>
+          Home
+        </button>
         <button onClick={()=>navigate('/members')} className='text-white relative bg-green-700 p-2 rounded'>add
           <div className='absolute h-6 w-6 flex justify-center items-center top-[-12px] right-[-2px] bg-red-700 p-1 rounded-full'>
             {members.length}
